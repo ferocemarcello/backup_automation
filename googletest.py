a692890ae1b5e8028f366a7c5a49400713c6bd52
Marcello Feroce <ferocemarcello@gmail.com>
2024-09-15 21:15:37 +0200

    working on upload to google

--- googletest.py
+++ googletest.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
 import httplib2
+import argparse
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
@@ -13,18 +14,30 @@ def get_credential(service_account_file:str, scopes:list):
 def get_service(service_account_file:str, scopes:list):
   return build('drive', 'v3', http=get_credential(scopes=scopes, service_account_file=service_account_file).authorize(httplib2.Http()))
 
+def upload_file_to_drive(service, file_path: str, file_name: str, folder_id: str):
+    file = service.files().create(body={'name': file_name, 'parents': [folder_id]}, media_body=MediaFileUpload(file_path), fields='id').execute()
+    print('File ID: %s' % file.get('id'))
 def main():
-  SCOPES = ['https://www.googleapis.com/auth/drive']
-  SERVICE_ACCOUNT_KEY_FILE = 'backupautomation_service_account_key.json'
+  #python upload_to_drive.py --folder_id <your_drive_folder_id> --file_path <path_to_your_file> --file_name <your_file_name> --service_account <path_to_service_account_key_file>
+  parser = argparse.ArgumentParser(description="Upload a file to Google Drive using a service account")
+  parser.add_argument('--folder_id', type=str, required=True, help="Google Drive folder ID")
+  parser.add_argument('--file_path', type=str, required=True, help="Path to the file to upload")
+  parser.add_argument('--file_name', type=str, default=None, help="Name of the file to upload to Google Drive")
+  parser.add_argument('--service_account', type=str, required=True, help="Path to the service account key file (JSON)")
+  
+  args = parser.parse_args()
+  if args.file_name is None:
+    args.file_name = os.path.basename(args.file_path)
 
+  # Define Google Drive API scopes
+  SCOPES = ['https://www.googleapis.com/auth/drive']
+  SERVICE_ACCOUNT_KEY_FILE = args.service_account  # Path to the service account key file
+
+  # Get authenticated Google Drive service
   service = get_service(scopes=SCOPES, service_account_file=SERVICE_ACCOUNT_KEY_FILE)
 
-  file_metadata = {'name': 'notion_workspace.json', 'parents': ['drive_folder']} # Replace with your file name
-  media = MediaFileUpload('./backup/notion_workspace.json') # Replace with your file path and mimetype
-  file = service.files().create(body=file_metadata,
-                                    media_body=media,
-                                    fields='id').execute()
-  print('File ID: %s' % file.get('id'))
+  # Upload the file to Google Drive
+  upload_file_to_drive(service=service, file_path=args.file_path, file_name=args.file_name, folder_id=args.folder_id)
 
 
 if __name__ == '__main__':
